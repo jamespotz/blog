784a71aab61ca6973ca32b673985c04395b7f643
James Rooke <thepoltergeist23@gmail.com>
2015-11-15 09:12:09 +0800

    add devise and more

--- Gemfile
+++ Gemfile
@@ -39,3 +39,5 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 
 gem 'simple_form'
+
+gem 'devise', '~> 3.5.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+
+  devise_for :users, :skip => :registrations
+  resources :posts do
+    resources :comments
+  end
+
   resources :home
   root 'home#index'
 end
